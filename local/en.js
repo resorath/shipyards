cb8dd34b765fd7fd9a1780342ad182a4d862b7eb
Sean Feil <sean@git.tophatandmonocle.com>
2018-11-12 15:49:43 -0700

    Basic descriptions

--- local/en.js
+++ local/en.js
@@ -1,7 +1,7 @@
 var locale = {
 	"ships": {
-		"Standard Fighter": "Once described as \"the perfect ship for a war on a budget\", the Standard Fighter is designed for mass production at the lowest cost to get weapons into space.\n\nEquipped with two low-power weapon hardpoints and a basic shield system, the Standard Fighter does well against unarmed civilian targets and poorly defended medical ships alike.",
-		"Longsword Corvette": "Its really\nlong"
+		"Standard Fighter": "Once described as \"the perfect ship for war on a budget\", the Standard Fighter is designed for mass production at the lowest cost to get weapons into space.\n\nEquipped with two low-power weapon hardpoints and a basic shield system, the Standard Fighter does well against unarmed civilian targets and poorly defended medical ships alike.",
+		"Longsword Corvette": "The Longsword Corvette is really long."
 	},
 
 	"weapons": {
